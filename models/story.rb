04e06885d72f554d885b361b6015571cbffc4d05
Daniel Lurie <daniel.lurie@erated.com>
2020-01-20 03:53:01 +0200

    scrape!

--- models/story.rb
+++ models/story.rb
@@ -7,20 +7,33 @@ class Story
   STATUSES = [
     'Pending',
     'Done',
-    'Error'
+    'Error',
+    nil
   ].freeze
 
   field :url, type: String
-  field :scrape_status, type: String, default: 'Pending'
+  field :scrape_status, type: String
+  field :meta_data, type: Hash
 
   validates :url, presence: true, uniqueness: true, format: { with: URI::regexp(%w(http https)) }
-  validates :scrape_status, presence: true, inclusion: { in: STATUSES }
+  validates :scrape_status, inclusion: { in: STATUSES }
 
   index({ url: 'text' }, { unique: true, name: 'url_index' })
 
   before_validation :assign_url, on: :create
 
+  def scrape!
+    self.meta_data = MetaParser.call(url)
+    self.scrape_status = result_status
+
+    save!
+  end
+
   protected
+
+  def result_status
+    meta_data.blank? ? 'Error' : 'Done'
+  end
 
   def assign_url
     # will not pass validation if url already exists
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -26,12 +26,71 @@ describe Story do
       expect(Story.create(url: '123')).to_not be_valid
     end
 
-    it 'validates scrape_status presence' do
-      expect(Story.new(url: 'http://www.example.com', scrape_status: nil)).to_not be_valid
+    it 'validates scrape_status in [Pending, Done, Error, nil]' do
+      expect(Story.new(url: 'http://www.example.com', scrape_status: 'xxx')).to_not be_valid
+    end
+  end
+
+  describe '#scrape!' do
+    context 'when success' do
+      let(:og_file) { File.expand_path '../../support/html/og.html', __FILE__  }
+
+      before do
+        allow_any_instance_of(MetaParser).to receive(:page).and_return(
+          OpenGraphReader.parse(
+            File.open(og_file).read
+          )
+        )
+      end
+
+      it 'assigns meta_data and sets scrape_status to done' do
+        story = Story.create!(url: 'http://www.example.com')
+
+        story.scrape!
+
+        expect(story.scrape_status).to eq('Done')
+        expect(story.meta_data).to eq(
+          {
+            url: 'http://ogp.me/',
+            title: 'Open Graph protocol',
+            type: 'website',
+            images: [
+              {
+                url: 'http://ogp.me/logo.png',
+                type: 'image/png',
+                width: 300,
+                height: 300,
+                alt: 'The Open Graph logo'
+              },
+              {
+                url: 'http://ogp.me/logo_1.png',
+                type: 'image/png',
+                width: 300,
+                height: 400,
+                alt: 'The Open Graph logo 1'
+              }
+            ]
+          }
+        )
+      end
     end
 
-    it 'validates scrape_status in [Pending, Done, Error]' do
-      expect(Story.new(url: 'http://www.example.com', scrape_status: 'xxx')).to_not be_valid
+    context 'when fail' do
+      let(:no_meta_file) { File.expand_path '../../support/html/no_meta.html', __FILE__  }
+
+      before do
+        allow_any_instance_of(MetaParser).to receive(:page).and_return(
+          OpenGraphReader.parse( File.open(no_meta_file).read)
+        )
+      end
+
+      it 'sets status to error' do
+        story = Story.create!(url: 'http://www.example.com')
+
+        story.scrape!
+
+        expect(story.scrape_status).to eq('Error')
+      end
     end
   end
 end
